87baf2c7aff4c2ae9e8d3017f1fab041f8a17fbd
Austin Schrader <austin.jschrader@gmail.com>
2020-11-09 11:58:50 -0800

    Add eslint configurations, and start/lint to our package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clearn-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: './src/main.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-	},
+  },
+  devtool: 'eval-source-map',
+  devServer:{
+    contentBase: './dist'
+  },
+
 	plugins: [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
@@ -21,7 +26,12 @@ module.exports = {
 			{
 				test: /\.css$/,
 				use: ['style-loader', 'css-loader'],
-			},
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader:"eslint-loader"
+      }
 		],
 	},
 };
